2c2c9722dce759fd5b6138a6f8b73c69fcc4fcb8
hyunsikshin <hyunsik.dev@gmail.com>
2018-07-02 15:02:00 +0900

    Customizing the Django Admin

--- nomadgram/images/admin.py
+++ nomadgram/images/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+
+    list_display_links = (
+        'location',
+    )
+
+    search_fields = (
+        'location',
+    )
+
+    list_filter = (
+        'location',
+        'creator',
+    )
+
+    list_display = (
+        'creator',
+        'file',
+        'location',
+        'caption',
+        'created_at',
+        'updated_at',
+    )
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'message',
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from nomadgram.users import models as user_models
-
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
+@python_2_unicode_compatible
 class TimeStampledModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,7 +12,7 @@ class TimeStampledModel(models.Model):
     class Meta: # This is not variable things
         abstract = True # Make abstarct base class option
 
-
+@python_2_unicode_compatible
 class Image(TimeStampledModel):
 
     """ Image Model """
@@ -21,6 +22,10 @@ class Image(TimeStampledModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null = True, on_delete = models.CASCADE)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+@python_2_unicode_compatible
 class Comment(TimeStampledModel):
 
     """ Comment Model """
@@ -29,9 +34,16 @@ class Comment(TimeStampledModel):
     creator = models.ForeignKey(user_models.User, null = True, on_delete = models.CASCADE)
     image = models.ForeignKey(Image, null = True, on_delete = models.CASCADE)
 
+    def __str__(self):
+        return self.message
+
+@python_2_unicode_compatible
 class Like(TimeStampledModel):
 
     """ Like Model """
 
     creator = models.ForeignKey(user_models.User, null = True, on_delete = models.CASCADE)
     image = models.ForeignKey(Image, null = True, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
