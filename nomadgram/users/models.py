22f1d3f12d7294ee2e4a09dabe97d8892e030d94
hyunsikshin <hyunsik.dev@gmail.com>
2018-07-02 12:43:18 +0900

    Creating Following:Follower Relationships

--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -17,12 +17,12 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
-
     website = models.URLField(null=True)
     bio = models.TextField(null=True)
     phone = models.CharField(max_length=140,null=True)
     gender = models.CharField(max_length=80 , choices=GENDER_CHOICES,null=True)
-    
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
