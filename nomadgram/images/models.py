53781fcae9c7f0bc8da466badf36cef51409f194
hyunsikshin <hyunsik.dev@gmail.com>
2018-07-02 11:10:16 +0900

    Creating the Image Model

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class TimeStampledModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta: # This is not variable things
+        abstract = True # Make abstarct base class option
+
+
+class Image(TimeStampledModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length = 140)
+    caption = models.TextField()
+
+class Comment(TimeStampledModel):
+
+    message = models.TextField()
