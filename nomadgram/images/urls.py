ff4863d65fd8d98ad610a1bb717d13ebb6215185
hyunsikshin <hyunsik.dev@gmail.com>
2018-07-04 16:55:31 +0900

    Viewing all the Comments and Likes

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -5,4 +5,6 @@ app_name = "images"
 
 urlpatterns = [
     path("all/", view=views.ListAllImages.as_view(), name="all_images"),
+    path("comments/", view=views.ListAllComments.as_view(), name="all_comments"),
+    path("likes/", view=views.ListAllLikes.as_view(), name="all_images"),
 ]
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -13,3 +13,25 @@ class ListAllImages(APIView):
         serializer = serializers.ImageSerializer(all_images, many=True)
 
         return Response(data=serializer.data)
+
+
+class ListAllComments(APIView):
+
+    def get(self, request, format = None):
+
+        all_comments = models.Comment.objects.all()
+
+        serializer = serializers.CommentSerializer(all_comments, many=True)
+
+        return Response(data=serializer.data)
+
+
+class ListAllLikes(APIView):
+
+    def get(self, request, format = None):
+
+        all_likes = models.Like.objects.all()
+
+        serializer = serializers.LikeSerializer(all_likes, many=True)
+
+        return Response(data=serializer.data)
